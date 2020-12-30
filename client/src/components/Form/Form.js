326fb86ef89363284ab9d71721049633431418b6
Trebilcode <d11a2n3i4e5l6@hotmail.com>
2020-12-30 15:17:49 -0500

    Create form, post and posts components with their propper styling; add axios to fetch the posts

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import useStyles from './styles'
+
+const Form = () => {
+  const classes = useStyles()
+  return (
+    
+  )
+}
+
+export default Form
---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import useStyles from './styles'
+
+const Post = () => {
+
+  const classes = useStyles()
+  return (
+    <div>
+      post
+    </div>
+  )
+}
+
+export default Post
