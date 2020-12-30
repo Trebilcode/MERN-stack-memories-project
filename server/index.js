af85def556648d5436d9d5b25339eb54cc33ccba
Trebilcode <d11a2n3i4e5l6@hotmail.com>
2020-12-29 20:07:14 -0500

    Create routes to get posts and schema and route to create it

--- server/index.js
+++ server/index.js
@@ -1,6 +1,28 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import cors from 'cors'
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
-const app = express()
+import postRoutes from './routes/posts.js';
 
+dotenv.config();
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose
+	.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() =>
+		app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+	)
+	.catch(error => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
